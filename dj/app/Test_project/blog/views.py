de85878df35ae7885faebf2873a21d2148464eb3
merklt62 <merklt62@gmail.com>
2019-04-30 12:43:17 +0300

    add Test_project

---
+++ dj/app/Test_project/blog/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import News
+
+
+def home(request):
+    data = {
+        'news': News.objects.all(),
+        'title': 'Главная страница блога'
+    }
+    return render(request, 'blog/home.html', data)
+
+class DeleteNewsView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = News
+    success_url = '/'
+
+    def test_func(self):
+        news = self.get_object()
+        if self.request.user == news.autor:
+            return True
+        return False
+
+class ShowNewsView(ListView):
+    model = News
+    template_name = 'blog/home.html'
+    context_object_name = 'news'
+    ordering = ['-date']
+    paginate_by = 2
+
+    def get_context_data(self, **kwards):
+        ctx = super(ShowNewsView, self).get_context_data(**kwards)
+        ctx['title'] = 'Главная страница блога'
+        return ctx
+
+class UserAllNewsView(ListView):
+    model = News
+    template_name = 'blog/user_news.html'
+    context_object_name = 'news'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return News.objects.filter(autor=user).order_by('-date')
+
+
+    def get_context_data(self, **kwards):
+        ctx = super(UserAllNewsView, self).get_context_data(**kwards)
+        ctx['title'] = f"Все статьи от пользователя {self.kwargs.get('username')}"
+        return ctx
+
+class NewsDetailView(DetailView):
+    model = News
+
+    def get_context_data(self, **kwards):
+        ctx = super(NewsDetailView, self).get_context_data(**kwards)
+        ctx['title'] = News.objects.filter(pk=self.kwargs['pk']).first()
+        return ctx
+
+class UpdateNewsView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = News
+    fields = ['title', 'text']
+
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        news = self.get_object()
+        if self.request.user == news.autor:
+            return True
+        return False
+
+class CreateNewsView(LoginRequiredMixin,CreateView):
+    model = News
+    fields = ['title', 'text']
+
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+
+def contacts(request):
+    return render(request, 'blog/contacts.html', {'title':'Страничка про нас'})
+
+# Create your views here.
---
+++ dj/app/Test_project/users/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserOurRegistration, ProfileImage, UserUpdateForm
+
+def register(request):
+    if request.method == "POST":
+        form = UserOurRegistration(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Аккаунт {username} был создан, введите имя пользователя и пароль для авторизации')
+            return redirect('user')
+    else:
+        form = UserOurRegistration()
+    return render(request, 'users/registration.html', {'form': form, 'title':'Регистрация пользователя'})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        img_profile = ProfileImage(request.POST, request.FILES, instance=request.user.profile)
+        update_user = UserUpdateForm(request.POST, instance=request.user)
+
+        if update_user.is_valid() and img_profile.is_valid():
+            update_user.save()
+            img_profile.save()
+            messages.success(request, f'Ваш аккаунт был обновлен')
+            return redirect('profile')
+    else:
+        img_profile = ProfileImage(instance=request.user.profile)
+        update_user = UserUpdateForm(instance=request.user)
+
+    data = {
+        'img_profile': img_profile,
+        'update_user': update_user
+    }
+
+    return render(request, 'users/profile.html', data)
+
+# Create your views here.
